aa4014b116636051495e9449a36ec4accf46c8f8
Damian Gryski <damian@gryski.com>
2012-12-15 23:43:44 +0100

    Add implementation of Marvin32

--- hashes_test.go
+++ hashes_test.go
@@ -288,6 +288,40 @@ var golden_superfast = []_Golden{
 	{0x494c35dd, "How can you write a big system without C++?  -Paul Glick"},
 }
 
+var golden_marvin = []_Golden{
+	{0xf7f2c954, ""},
+	{0xd46e71f7, "a"},
+	{0xb40c651c, "ab"},
+	{0x5b3bc23d, "abc"},
+	{0x6b15e57b, "abcd"},
+	{0x601e6ea8, "abcde"},
+	{0xfc18bd2c, "abcdef"},
+	{0x79b01bfb, "abcdefg"},
+	{0x54793238, "abcdefgh"},
+	{0xebf98191, "abcdefghi"},
+	{0x68a8001d, "abcdefghij"},
+	{0x659105c1, "Discard medicine more than two years old."},
+	{0xb98b31d, "He who has a shady past knows that nice guys finish last."},
+	{0xbae17c9a, "I wouldn't marry him with a ten foot pole."},
+	{0x9a299f69, "Free! Free!/A trip/to Mars/for 900/empty jars/Burma Shave"},
+	{0xb463d704, "The days of the digital watch are numbered.  -Tom Stoppard"},
+	{0xe6059c5f, "Nepal premier won't resign."},
+	{0xbdd4f772, "For every action there is an equal and opposite government program."},
+	{0x12af7ede, "His money is twice tainted: 'taint yours and 'taint mine."},
+	{0x1e9cae8, "There is no reason for any individual to have a computer in their home. -Ken Olsen, 1977"},
+	{0xcb683e33, "It's a tiny change to the code and not completely disgusting. - Bob Manchek"},
+	{0x2074fbfa, "size:  a.out:  bad magic"},
+	{0x52abb615, "The major problem is with sendmail.  -Mark Horton"},
+	{0x5a509711, "Give me a rock, paper and scissors and I will move the world.  CCFestoon"},
+	{0xf97f5273, "If the enemy is within range, then so are you."},
+	{0x494c0cb, "It's well we cannot hear the screams/That we create in others' dreams."},
+	{0x7150a3c0, "You remind me of a TV show, but that's all right: I watch it anyway."},
+	{0xc5f56430, "C is as portable as Stonehedge!!"},
+	{0x712bcf01, "Even if I could be Shakespeare, I think I should still choose to be Faraday. - A. Huxley"},
+	{0xedd44de6, "The fugacity of a constituent in a mixture of gases at a given temperature is proportional to its mole fraction.  Lewis-Randall Rule"},
+	{0xd9440105, "How can you write a big system without C++?  -Paul Glick"},
+}
+
 func TestJava(t *testing.T) {
 	testGolden(t, NewJava32(), golden_java, "java")
 }
@@ -333,6 +367,16 @@ func TestSuperFastHash(t *testing.T) {
 	testIncremental(t, m, 0x54de96ed, "superfast")
 
 	testGolden(t, m, golden_superfast, "superfast")
+}
+
+func TestMarvin(t *testing.T) {
+
+	m := NewMarvin32(0x5D70D359C498B3F8) // seed for testing
+
+	// test the incremental hashing logic
+	testIncremental(t, m, 0x28685e7a, "marvin")
+
+	testGolden(t, m, golden_marvin, "marvin")
 }
 
 func BenchmarkJava32(b *testing.B) {
---
+++ marvin.go
@@ -0,0 +1,147 @@
+// This file is an implementation of the InternalMarvin32HashString hash function.
+// The code is based on the implementation found at https://github.com/floodyberry/Marvin32
+
+// This implementation Copyright (c) 2011 Damian Gryski <damian@gryski.com>
+// Licensed under the GPLv3, or at your option any later version
+
+package dgohash
+
+import (
+	"hash"
+)
+
+type marvin struct {
+	seed   uint64
+	lo, hi uint32
+	t      [4]byte // as-yet-unprocessed bytes
+	rem    int     // how many bytes in t[] are valid
+}
+
+func (st *marvin) update(v uint32) {
+	st.lo += v
+	st.hi ^= st.lo
+	st.lo = rotl32(st.lo, 20) + st.hi
+	st.hi = rotl32(st.hi, 9) ^ st.lo
+	st.lo = rotl32(st.lo, 27) + st.hi
+	st.hi = rotl32(st.hi, 19)
+}
+
+func marvin_update(lo, hi, v uint32) (uint32, uint32) {
+	lo += v
+	hi ^= lo
+	lo = rotl32(lo, 20) + hi
+	hi = rotl32(hi, 9) ^ lo
+	lo = rotl32(lo, 27) + hi
+	hi = rotl32(hi, 19)
+	return lo, hi
+}
+
+func NewMarvin32(seed uint64) hash.Hash32 {
+	m := new(marvin)
+	m.seed = seed
+	m.Reset()
+	return m
+}
+
+func (m *marvin) Size() int      { return 4 }
+func (m *marvin) BlockSize() int { return 4 }
+func (m *marvin) Reset()         { m.lo = uint32(m.seed); m.hi = uint32(m.seed >> 32); m.rem = 0 }
+
+func (m *marvin) Write(data []byte) (int, error) {
+
+	length := len(data)
+
+	// Since the hash actually processes uint32s, but we allow []byte to be
+	// Written, we have to keep track of the tail bytes that haven't yet
+	// been processed, and do that on next round if we can scrounge
+	// together a uint32.  If they're not merged here, they're pulled in
+	// during the finalize step
+	if m.rem != 0 {
+
+		need := 4 - m.rem
+
+		if length < need {
+			copy(m.t[m.rem:], data[:length])
+			m.rem += length
+
+			return length, nil
+		}
+
+		var k1 uint32
+
+		switch need {
+		case 1:
+			k1 = uint32(m.t[0]) | uint32(m.t[1])<<8 | uint32(m.t[2])<<16 | uint32(data[0])<<24
+		case 2:
+			k1 = uint32(m.t[0]) | uint32(m.t[1])<<8 | uint32(data[0])<<16 | uint32(data[1])<<24
+		case 3:
+			k1 = uint32(m.t[0]) | uint32(data[0])<<8 | uint32(data[1])<<16 | uint32(data[2])<<24
+		}
+
+		m.update(k1)
+
+		// we've used up some bytes
+		length -= need
+		// nothing is left in the tail
+		m.rem = 0
+		data = data[need:]
+	}
+
+	// figure out the length of the tail, and round down b
+	rem := length & 3
+	b := length - rem
+
+	for i := 0; i < b; i += 4 {
+		k1 := uint32(data[i]) | uint32(data[i+1])<<8 | uint32(data[i+2])<<16 | uint32(data[i+3])<<24
+		m.update(k1)
+	}
+
+	// copy the tail for later
+	copy(m.t[:rem], data[b:])
+
+	m.rem = rem
+
+	return length, nil
+}
+
+func (m *marvin) Sum(b []byte) []byte {
+	h1 := m.Sum32()
+	p := make([]byte, 4)
+	p[0] = byte(h1 >> 24)
+	p[1] = byte(h1 >> 16)
+	p[2] = byte(h1 >> 8)
+	p[3] = byte(h1)
+	if b == nil {
+		return p
+	}
+	return append(b, p...)
+}
+
+// marvin finalize step
+func (m *marvin) Sum32() uint32 {
+
+	/* pad the final 0-3 bytes with 0x80 */
+	final := uint32(0x80)
+
+	// copy so as not to change the internal state
+	lo := m.lo
+	hi := m.hi
+
+	switch m.rem {
+
+	case 3:
+		final = (final << 8) | uint32(m.t[2])
+		fallthrough
+	case 2:
+		final = (final << 8) | uint32(m.t[1])
+		fallthrough
+	case 1:
+		final = (final << 8) | uint32(m.t[0])
+		fallthrough
+	}
+
+	lo, hi = marvin_update(lo, hi, final)
+	lo, hi = marvin_update(lo, hi, 0)
+
+	return lo ^ hi
+}
